24e0e946f3f001d4249d5a3b849f67c5a8f3f03f
Malik <deepakm661@gmail.com>
2019-01-15 00:34:36 +0530

    error handled

--- server.js
+++ server.js
@@ -18,16 +18,22 @@ app.post('/' , (req,res) => {
        let city = req.body.city_name
        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=83140eda048e6595136fe99194c5a9ea`
        request(url,function(error,response,body) {
-                   let weather_json = JSON.parse(body)   
-                   console.log(weather_json)               
-                   let weather = {
-                       city:city,
-                       temperature:Math.round(weather_json.main.temp),
-                       description:weather_json.weather[0].description,
-                       icon:weather_json.weather[0].icon
-                   }
-                   let weather_data = {weather:weather}
-       res.render('index' ,{weather_data})  
+                   let weather_json = JSON.parse(body)  
+                   console.log(weather_json) 
+                  if(weather_json.main === undefined) {
+                     res.send('City Not Found')
+                  }      
+                  else {
+                    let weather = {
+                        city:city,
+                        temperature:Math.round(weather_json.main.temp),
+                        description:weather_json.weather[0].description,
+                        icon:weather_json.weather[0].icon
+                    }
+                    let weather_data = {weather:weather}
+                      res.render('index' ,{weather_data}) 
+                  }        
+                   
        })
 })
 
